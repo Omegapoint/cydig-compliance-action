aed17130b957e63093b5b05b16159b63a4df159c
Jonathan Bokvad Engarås <jonathan.bokvad.engaras@omegapoint.se>
2023-09-07 13:08:14 +0200

    test octakit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core';
-
+import * as github from '@actions/github';
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
@@ -7,6 +7,15 @@ import * as core from '@actions/core';
 export async function run(): Promise<void> {
   try {
     console.log('Custom task is working!');
+
+    const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+    const { owner, repo } = github.context.repo;
+    const branchProtection = await octokit.rest.repos.getBranchProtection({
+      owner: owner,
+      repo: repo,
+      branch: 'main',
+    });
+ console.log(branchProtection.data);
 
   } catch (error) {
     // Fail the workflow run if an error occurs
