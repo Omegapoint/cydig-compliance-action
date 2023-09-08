18057f039c41f922999895ce10b89212403c6d91
Jonathan Bokvad Engarås <jonathan.bokvad.engaras@omegapoint.se>
2023-09-08 09:38:46 +0200

    Added logging

--- src/threatmodeling/ThreatModelingService.ts
+++ src/threatmodeling/ThreatModelingService.ts
@@ -1,20 +1,15 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-export class ThreatModelingService{
+export class ThreatModelingService {
+  public static async getStateOfThreatModeling(threatModelingDate: { date: string; boardsTag: string }): Promise<void> {
+    console.log('Running Threat Modeling Controls');
 
-    public static async getStateOfThreatModeling(threatModelingDate: { 
-        date: string;
-        boardsTag: string;
-    }): Promise<void> {
-        console.log('Running Threat Modeling Controls');
-        
-        if(!threatModelingDate.date || threatModelingDate.date === "date-of-threat-modeling"){
-            core.warning("Threat Modeling Date is not set!");
-            return;
-        }
-        core.setOutput("threatModelingDate", threatModelingDate.date);
-
-        console.log("threatModelingDate is set", threatModelingDate.date);
-
+    if (!threatModelingDate.date || threatModelingDate.date === 'date-of-threat-modeling') {
+      core.warning('Threat Modeling Date is not set!');
+      return;
     }
+    core.setOutput('threatModelingDate', threatModelingDate.date);
+
+    console.log('threatModelingDate is set', threatModelingDate.date);
+  }
 }
