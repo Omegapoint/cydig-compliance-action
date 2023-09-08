03961c6f1e7ec08d9451ed3dbdf88424fee2ad29
Jonathan Bokvad Engarås <jonathan.bokvad.engaras@omegapoint.se>
2023-09-08 08:20:35 +0200

    test

--- src/branchprotection/BranchProtectionService.ts
+++ src/branchprotection/BranchProtectionService.ts
@@ -1,30 +1,32 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-export class BranchProtectionService{
+export class BranchProtectionService {
+  public static async getStateOfBranchProtection(): Promise<void> {
+    console.log('Custom task is working!');
+    const token = core.getInput('repo-token');
+    console.log('Got the token');
 
-    public static async getStateOfBranchProtection(): Promise<void> {
-        console.log('Custom task is working!');
-        const token = core.getInput('repo-token');
-        console.log("Got the token");
+    const octokit = github.getOctokit(token);
+    console.log('octoKit authenticated');
 
-        const octokit = github.getOctokit(token);
-        console.log("octoKit authenticated");
+    const { owner, repo } = github.context.repo;
+    console.log(`Owner: ${owner}`);
+    console.log(`Repo: ${repo}`);
 
-        const { owner, repo } = github.context.repo;
-        console.log(`Owner: ${owner}`);
-        console.log(`Repo: ${repo}`);
-        
-        console.log("Going to get branch protection");
-        await octokit.rest.repos.getBranchProtection({
+    console.log('Going to get branch protection');
+    await octokit.rest.repos
+      .getBranchProtection({
         owner: owner,
         repo: repo,
         branch: 'main',
-        }).then((response) => { 
-            console.log("Got the branch protection");
-            console.log(response.data);
-        }).catch((error) => {
-            core.warning("Error getting branch protection!");
-            core.warning("Error: ", error);
-        });
-    }
+      })
+      .then((response) => {
+        console.log('Got the branch protection');
+        console.log(response.data);
+      })
+      .catch((error) => {
+        core.warning('Error getting branch protection!');
+        console.log('Error: ', error);
+      });
+  }
 }
