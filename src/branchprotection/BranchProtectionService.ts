4dbd0515802eb3fb68733e00361123ae756946b5
Johanna Ahlskog <johanna.ahlskog@omegapoint.com>
2023-09-14 14:49:58 +0200

    added numberOfReviewers

--- src/branchprotection/BranchProtectionService.ts
+++ src/branchprotection/BranchProtectionService.ts
@@ -3,17 +3,14 @@ import * as github from '@actions/github';
 export class BranchProtectionService {
   public static async getStateOfBranchProtection(): Promise<void> {
     console.log('\n Running branch protection control');
-    const token = core.getInput('repo-token');
-    //console.log('Got the token');
+    
+    //numberOfReviewers > 0, if state of branch protection changes
+    let numberOfReviewers: number = 0;
 
+    const token = core.getInput('repo-token');
     const octokit = github.getOctokit(token);
-    //console.log('octoKit authenticated');
 
     const { owner, repo } = github.context.repo;
-    //console.log(`Owner: ${owner}`);
-    //console.log(`Repo: ${repo}`);
-
-    //console.log('Going to get branch protection');
     await octokit.rest.repos
       .getBranchProtection({
         owner: owner,
@@ -21,13 +18,11 @@ export class BranchProtectionService {
         branch: 'main',
       })
       .then((response) => {
-        //console.log('Got the branch protection');
         console.log(response.data);
       })
       .catch((error) => {
         console.log('Branch protections is not enabled for repository: ' + repo)
-        //core.warning('Error getting branch protection!');
-        //console.log('Error: ', error.message);
       });
+      core.exportVariable('numberOfReviewers', numberOfReviewers)
   }
 }
