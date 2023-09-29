fd983996b08cba890b6801f59d339721d9427332
Johanna Ahlskog <johanna.ahlskog@omegapoint.com>
2023-09-29 11:12:54 +0200

    fixed env. variableds

--- src/threatmodeling/ThreatModelingService.ts
+++ src/threatmodeling/ThreatModelingService.ts
@@ -1,16 +1,14 @@
 import * as core from '@actions/core';
 export class ThreatModelingService {
   public static async getStateOfThreatModeling(threatModeling: { date: string; boardsTag: string }): Promise<void> {
-    let threatModelingDate: string = process.env.threatModelingDate || '';
-    console.log("(before if) process.env.threatModelingDate: " + process.env.threatModelingDate)
-    console.log("(before if) threatModelingDate:" + threatModelingDate)
-    if (threatModelingDate) {
+    if (process.env.threatModelingDate) {
+      core.exportVariable('threatModelingDate',  process.env.threatModelingDate);
+    } else {
       if (!threatModeling.date || threatModeling.date === 'date-of-threat-modeling') {
         core.warning('Threat Modeling Date is not set!');
         return;
       }
-      threatModelingDate = threatModeling.date;
+      core.exportVariable('threatModelingDate',  threatModeling.date);
     }
-    core.exportVariable('threatModelingDate', threatModelingDate);
   }
 }
