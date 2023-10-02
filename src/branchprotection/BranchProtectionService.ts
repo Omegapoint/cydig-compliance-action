51b633c50330816680da8d7223e36f75a3a7f2a2
Jonathan Bokvad Engarås <jonathan.bokvad.engaras@omegapoint.se>
2023-10-02 15:38:35 +0200

    refactor project

--- src/branchprotection/BranchProtectionService.ts
+++ src/branchprotection/BranchProtectionService.ts
@@ -7,8 +7,9 @@ export class BranchProtectionService {
 
     const { owner, repo }: { owner: string; repo: string } = github.context.repo;
     const token: string = core.getInput('PAT-token');
-    const octokit: any = github.getOctokit(token);
-
+    // eslint-disable-next-line @typescript-eslint/typedef
+    const octokit = github.getOctokit(token);
+    let numberOfReviewers: number = 0;
     await octokit.rest.repos
       .getBranchProtection({
         owner: owner,
@@ -16,19 +17,20 @@ export class BranchProtectionService {
         branch: 'main',
       })
       .then((response: any) => {
-        console.log('Response', response);
-        console.log(
-          'Branch protection is enabled, number of reviewers are:',
-          response.data.required_pull_request_reviews?.['required_approving_review_count']
-        );
-        core.exportVariable(
-          'numberOfReviewers',
-          response.data.required_pull_request_reviews?.['required_approving_review_count']
-        );
+        console.log('Response', response.data);
+        if (!response.data.enforce_admins?.enabled) {
+          console.log('Branch protection can be overridden by admins, and is therefore counted as not enabled');
+          return;
+        }
+        if (response.data.required_pull_request_reviews?.['required_approving_review_count'] >= 1) {
+          numberOfReviewers = response.data.required_pull_request_reviews?.['required_approving_review_count'];
+          console.log('Branch protection is enabled, number of reviewers are:', numberOfReviewers);
+        }
       })
       .catch((error: any) => {
-        console.log('Branch protections is not enabled for repository: ' + repo);
         console.log('Error:', error?.message);
       });
+    if (numberOfReviewers <= 0) console.log('Branch protections is not enabled for repository: ' + repo);
+    core.exportVariable('numberOfReviewers', numberOfReviewers);
   }
 }
