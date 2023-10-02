066f0ecb6aaec8f121c86f61ca9e1fe10f6d28bc
Jonathan Bokvad Engarås <jonathan.bokvad.engaras@omegapoint.se>
2023-10-02 16:02:58 +0200

    refactor project

--- src/branchprotection/BranchProtectionService.ts
+++ src/branchprotection/BranchProtectionService.ts
@@ -1,36 +1,36 @@
+/* eslint-disable @typescript-eslint/typedef */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 export class BranchProtectionService {
   public static async getStateOfBranchProtection(): Promise<void> {
-    console.log('\n Running branch protection control');
-
-    const { owner, repo }: { owner: string; repo: string } = github.context.repo;
-    const token: string = core.getInput('PAT-token');
-    // eslint-disable-next-line @typescript-eslint/typedef
-    const octokit = github.getOctokit(token);
-    let numberOfReviewers: number = 0;
-    await octokit.rest.repos
-      .getBranchProtection({
-        owner: owner,
-        repo: repo,
+    try {
+      console.log('\n Running branch protection control');
+      const { owner, repo }: { owner: string; repo: string } = github.context.repo;
+      const token: string = core.getInput('PAT-token');
+      const octokit = github.getOctokit(token);
+      const response = await octokit.rest.repos.getBranchProtection({
+        owner,
+        repo,
         branch: 'main',
-      })
-      .then((response: any) => {
-        console.log('Response', response.data);
-        if (!response.data.enforce_admins?.enabled) {
-          console.log('Branch protection can be overridden by admins, and is therefore counted as not enabled');
-          return;
-        }
-        if (response.data.required_pull_request_reviews?.['required_approving_review_count'] >= 1) {
-          numberOfReviewers = response.data.required_pull_request_reviews?.['required_approving_review_count'];
-          console.log('Branch protection is enabled, number of reviewers are:', numberOfReviewers);
-        }
-      })
-      .catch((error: any) => {
-        console.log('Error:', error?.message);
       });
-    if (numberOfReviewers <= 0) console.log('Branch protections is not enabled for repository: ' + repo);
-    core.exportVariable('numberOfReviewers', numberOfReviewers);
+      const numberOfReviewers: number =
+        response.data.required_pull_request_reviews?.required_approving_review_count || 0;
+      console.log('Response', response.data);
+
+      if (response.data.enforce_admins?.enabled === false) {
+        console.log('Branch protection can be overridden by admins and is therefore counted as not enabled');
+      }
+
+      if (numberOfReviewers >= 1 && response.data.enforce_admins?.enabled === true) {
+        console.log('Branch protection is enabled, number of reviewers:', numberOfReviewers);
+      } else {
+        console.log('Branch protection is not enabled for repository:', repo);
+      }
+
+      core.exportVariable('numberOfReviewers', numberOfReviewers);
+    } catch (error) {
+      console.log('Error:', error.message);
+    }
   }
 }
