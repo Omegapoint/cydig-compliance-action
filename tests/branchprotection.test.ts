eafa00a30ad38ee51b22853c1141e726c6928f53
Jonathan Bokvad Engarås <jonathan.bokvad.engaras@omegapoint.se>
2023-10-03 11:02:43 +0200

    refactor project

---
+++ tests/branchprotection.test.ts
@@ -0,0 +1,49 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import sinon, { SinonSandbox, SinonStub } from 'sinon';
+import { expect } from 'chai';
+import { BranchProtectionService } from '../src/branchprotection/BranchProtectionService';
+describe('BranchProtectionService', () => {
+  let sandbox: SinonSandbox;
+  let getInputStub: SinonStub;
+  let warningStub: SinonStub;
+  let exportVariableStub: SinonStub;
+  let getOctokitStub: SinonStub;
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+    getInputStub = sandbox.stub(core, 'getInput');
+    warningStub = sandbox.stub(core, 'warning');
+    exportVariableStub = sandbox.stub(core, 'exportVariable');
+    getOctokitStub = sandbox.stub(github, 'getOctokit');
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should handle successful branch protection retrieval', async () => {
+    getInputStub.withArgs('PAT-token').returns('your-pat-token'); // Replace with your token
+
+    getOctokitStub.returns({
+      rest: {
+        repos: {
+          getBranchProtection: sinon.stub().resolves({
+            data: {
+              enforce_admins: { enabled: true },
+              required_pull_request_reviews: { required_approving_review_count: 1 },
+            },
+          }),
+        },
+      },
+    });
+
+    await BranchProtectionService.getStateOfBranchProtection();
+
+    expect(getInputStub.calledWith('PAT-token')).to.be.true;
+    expect(warningStub.called).to.be.false;
+    expect(exportVariableStub.calledWith('numberOfReviewers', 1)).to.be.true;
+  });
+
+  // Add more test cases for different scenarios as needed
+});
