71af057c18b220014cc4da88f162bd29c055a917
Linnéa Oxenwaldt <linnea.oxenwaldt@basalt.se>
2024-02-28 09:50:29 +0100

    adding name of tool

--- src/sasttools/SastService.ts
+++ src/sasttools/SastService.ts
@@ -1,18 +1,18 @@
 import * as core from '@actions/core';
 
 export class SastService {
-  public static async getStateOfSastTool(sastTool: string): Promise<void> {
+  public static async getStateOfSastTool(sastTool: { nameOfTool: string }): Promise<void> {
     console.log('\n Running SAST control');
     if (process.env.sastTool) {
       console.log(`SAST Tool: ${process.env.sastTool}`);
       core.exportVariable('sastTool', process.env.sastTool);
     } else {
-      if (!sastTool || sastTool === 'name-of-tool') {
+      if (!sastTool.nameOfTool || sastTool.nameOfTool === 'name-of-tool') {
         core.warning('SAST Tool is not set!');
         return;
       }
-      console.log(`SAST Tool: ${sastTool}`);
-      core.exportVariable('sastTool', sastTool);
+      console.log(`SAST Tool: ${sastTool.nameOfTool}`);
+      core.exportVariable('sastTool', sastTool.nameOfTool);
     }
   }
 }
--- src/scatools/ScaService.ts
+++ src/scatools/ScaService.ts
@@ -1,18 +1,18 @@
 import * as core from '@actions/core';
 
 export class ScaService {
-  public static async getStateOfScaTool(scaTool: string): Promise<void> {
+  public static async getStateOfScaTool(scaTool: {nameOfTool: string}): Promise<void> {
     console.log('\n Running SCA control');
     if (process.env.scaTool) {
       console.log(`SCA Tool: ${process.env.scaTool}`);
       core.exportVariable('scaTool', process.env.scaTool);
     } else {
-      if (!scaTool || scaTool === 'name-of-tool') {
+      if (!scaTool.nameOfTool || scaTool.nameOfTool === 'name-of-tool') {
         core.warning('SCA Tool is not set!');
         return;
       }
-      console.log(`SCA Tool: ${scaTool}`);
-      core.exportVariable('scaTool', scaTool);
+      console.log(`SCA Tool: ${scaTool.nameOfTool}`);
+      core.exportVariable('scaTool', scaTool.nameOfTool);
     }
   }
 }
