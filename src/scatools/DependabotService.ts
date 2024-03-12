7f9f87f8398c3a8fa3dd101d0dfc0ea5f12d309f
hampus-andersson-op <hampus.andersson@omegapoint.se>
2024-03-12 11:11:16 +0100

    Remove old code

--- src/scatools/DependabotService.ts
+++ src/scatools/DependabotService.ts
@@ -7,8 +7,6 @@ export class DependabotService {
 
         const { owner, repo }: { owner: string; repo: string } = github.context.repo;
         const token: string = core.getInput('PAT-token');
-        // type dependabotAlertType = Endpoints['GET /repos/{owner}/{repo}/dependabot/alerts']['response'];
-        // const octokit: InstanceType<typeof GitHub> = github.getOctokit(token);
         const octokit = new Octokit({
             auth: token
         });
@@ -19,7 +17,7 @@ export class DependabotService {
                 owner: owner,
                 repo: repo,
                 per_page: 100,
-                // state: "open"
+                state: "open"
             }
         )
         
@@ -47,33 +45,6 @@ export class DependabotService {
             }
         }
         
-        // const alerts: any = await octokit.paginate(
-        //     "GET /repos/{owner}/{repo}/dependabot/alerts",
-        //     {
-        //       owner: owner,
-        //       repo: repo,
-        //       per_page: 100,
-        //     });
-    
-        // for (const alert of alerts){
-        //     for(const a of alert.data) {
-        //     }
-        //     switch (alert){
-        //         case "low":
-        //             scaNumberOfSeverity1 ++
-        //             break
-        //         case "medium":
-        //             scaNumberOfSeverity2 ++
-        //             break
-        //         case "high":
-        //             scaNumberOfSeverity3 ++
-        //             break
-        //         case "critical":
-        //             scaNumberOfSeverity4 ++
-        //             break
-        //     }
-        // }
-        
         console.log(scaNumberOfSeverity1);
         console.log(scaNumberOfSeverity2);
         console.log(scaNumberOfSeverity3);
@@ -85,41 +56,4 @@ export class DependabotService {
         core.exportVariable('scaNumberOfSeverity3', scaNumberOfSeverity3);
         core.exportVariable('scaNumberOfSeverity4', scaNumberOfSeverity4);
     }
-
-    // public static async setDependabotFindings2(): Promise<void> {
-
-    //     const { owner, repo }: { owner: string; repo: string } = github.context.repo;
-    //     const token: string = core.getInput('PAT-token');
-
-    //     const octokit: InstanceType<typeof GitHub> = github.getOctokit(token);
-    //     type dependabotAlertType = Endpoints['GET /repos/{owner}/{repo}/dependabot/alerts']['response'];
-
-    //     const alerts: Array<dependabotAlertType> = await octokit.paginate(
-    //         "GET /repos/{owner}/{repo}/dependabot/alerts",
-    //         {
-    //           owner: owner,
-    //           repo: repo,
-    //           per_page: 100,
-    //         });
-
-    //     const severeties = alerts.flatMap((alert) => DependabotService.GetSeverity(alert));
-        
-    //     const lowSeverityCount = DependabotService.severityCountForGivenSeverity(severeties, "low");
-    //     const mediumSeverityCount = DependabotService.severityCountForGivenSeverity(severeties, "medium");
-    //     const highwSeverityCount = DependabotService.severityCountForGivenSeverity(severeties, "high");
-    //     const criticalSeverityCount = DependabotService.severityCountForGivenSeverity(severeties, "critical");
-
-    //     core.exportVariable('scaNumberOfSeverity1', lowSeverityCount);
-    //     core.exportVariable('scaNumberOfSeverity2', mediumSeverityCount);
-    //     core.exportVariable('scaNumberOfSeverity3', highwSeverityCount);
-    //     core.exportVariable('scaNumberOfSeverity4', criticalSeverityCount);
-    // }
-
-    // private static severityCountForGivenSeverity(severeties: ("low" | "medium" | "high" | "critical")[], severity: string) {
-    //     return severeties.filter((allert) => allert === severity).length;
-    // }
-
-    // private static GetSeverity(alert): "low" | "medium" | "high" | "critical" | readonly ("low" | "medium" | "high" | "critical")[] {
-    //     return alert.data.flatMap((data) => data.security_vulnerability.severity);
-    // }
   }
