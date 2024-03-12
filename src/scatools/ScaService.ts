b128b732e3e763b275e548d45ff7d95b9565f015
hampus-andersson-op <hampus.andersson@omegapoint.se>
2024-03-12 14:26:54 +0100

    Clean up flow

--- src/scatools/ScaService.ts
+++ src/scatools/ScaService.ts
@@ -4,21 +4,20 @@ import { DependabotService } from './DependabotService';
 export class ScaService {
   public static async getStateOfScaTool(scaTool: { nameOfTool: string }): Promise<void> {
     console.log('\n Running SCA control');
+    let sca = scaTool.nameOfTool
     if (process.env.scaTool) {
-      console.log(`SCA Tool: ${process.env.scaTool}`);
-      core.exportVariable('scaTool', process.env.scaTool);
-    } else {
-      if (!scaTool.nameOfTool || scaTool.nameOfTool === 'name-of-tool') {
-        core.warning('SCA Tool is not set!');
-        return;
-      }
+      sca = process.env.scaTool
+    }
+    console.log(`SCA Tool: ${sca}`);
+    core.exportVariable('scaTool', sca);
+    
+    if (!sca || sca === "" || sca === 'name-of-tool') {
+    core.warning('SCA Tool is not set!');
+      return;
+    }
 
-      if(scaTool.nameOfTool === "dependabot"){
-        DependabotService.setDependabotFindings()
-      }
-
-      console.log(`SCA Tool: ${scaTool.nameOfTool}`);
-      core.exportVariable('scaTool', scaTool.nameOfTool);
+    if(sca === "dependabot"){
+      DependabotService.setDependabotFindings()
     }
   }
 }
