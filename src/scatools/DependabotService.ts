dbcc5583ecadc86965fad63bae02fc9c041ac457
hampus-andersson-op <hampus.andersson@omegapoint.se>
2024-03-12 10:27:44 +0100

    Use octokit/rest sdk

--- src/scatools/DependabotService.ts
+++ src/scatools/DependabotService.ts
@@ -2,28 +2,36 @@ import * as github from '@actions/github';
 import { GitHub } from '@actions/github/lib/utils';
 import * as core from '@actions/core';
 import { Endpoints } from '@octokit/types';
+import { Octokit } from '@octokit/rest';
 
 export class DependabotService {
     public static async setDependabotFindings(): Promise<void> {
 
         const { owner, repo }: { owner: string; repo: string } = github.context.repo;
         const token: string = core.getInput('PAT-token');
-        const octokit: InstanceType<typeof GitHub> = github.getOctokit(token);
+        // type dependabotAlertType = Endpoints['GET /repos/{owner}/{repo}/dependabot/alerts']['response'];
+        // const octokit: InstanceType<typeof GitHub> = github.getOctokit(token);
+        const octokit = new Octokit({
+            auth: token
+        });
         
-        const alerts: any = await octokit.paginate(
-            "GET /repos/{owner}/{repo}/dependabot/alerts",
+        const iterator = octokit.paginate.iterator(
+            octokit.dependabot.listAlertsForRepo,
             {
-              owner: owner,
-              repo: repo,
-              per_page: 100,
-            });
-
-            let scaNumberOfSeverity1: number = 0 
-            let scaNumberOfSeverity2: number = 0
-            let scaNumberOfSeverity3: number = 0
-            let scaNumberOfSeverity4: number = 0
-
-            for (const alert of alerts){
+                owner: owner,
+                repo: repo,
+                per_page: 100,
+                state: "open"
+            }
+        )
+        
+        let scaNumberOfSeverity1: number = 0 
+        let scaNumberOfSeverity2: number = 0
+        let scaNumberOfSeverity3: number = 0
+        let scaNumberOfSeverity4: number = 0
+        
+        for await (const {data: alerts } of iterator) {
+            for(const alert of alerts) {
                 switch (alert.security_vulnerability.severity){
                     case "low":
                         scaNumberOfSeverity1 ++
@@ -39,11 +47,39 @@ export class DependabotService {
                         break
                 }
             }
+        }
+        
+        // const alerts: any = await octokit.paginate(
+        //     "GET /repos/{owner}/{repo}/dependabot/alerts",
+        //     {
+        //       owner: owner,
+        //       repo: repo,
+        //       per_page: 100,
+        //     });
+    
+        // for (const alert of alerts){
+        //     for(const a of alert.data) {
+        //     }
+        //     switch (alert){
+        //         case "low":
+        //             scaNumberOfSeverity1 ++
+        //             break
+        //         case "medium":
+        //             scaNumberOfSeverity2 ++
+        //             break
+        //         case "high":
+        //             scaNumberOfSeverity3 ++
+        //             break
+        //         case "critical":
+        //             scaNumberOfSeverity4 ++
+        //             break
+        //     }
+        // }
 
-            core.exportVariable('scaNumberOfSeverity1', scaNumberOfSeverity1);
-            core.exportVariable('scaNumberOfSeverity2', scaNumberOfSeverity2);
-            core.exportVariable('scaNumberOfSeverity3', scaNumberOfSeverity3);
-            core.exportVariable('scaNumberOfSeverity4', scaNumberOfSeverity4);
+        core.exportVariable('scaNumberOfSeverity1', scaNumberOfSeverity1);
+        core.exportVariable('scaNumberOfSeverity2', scaNumberOfSeverity2);
+        core.exportVariable('scaNumberOfSeverity3', scaNumberOfSeverity3);
+        core.exportVariable('scaNumberOfSeverity4', scaNumberOfSeverity4);
     }
 
     public static async setDependabotFindings2(): Promise<void> {
