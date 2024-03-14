4c2580be24e8aea69e8c0729d34668e632fd4bf8
salemxd <uzumaki.salem@gmail.com>
2024-03-14 10:43:09 +0100

    added control to sast service

--- src/sasttools/CodeQLService.ts
+++ src/sasttools/CodeQLService.ts
@@ -2,8 +2,8 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 import { Octokit } from '@octokit/rest';
 
-export class DependabotService {
-  public static async setDependabotFindings(): Promise<void> {
+export class CodeQLService {
+  public static async setCodeQLFindings(): Promise<void> {
     try{
         const { owner, repo }: { owner: string; repo: string } = github.context.repo;
         const token: string = core.getInput('PAT-token');
@@ -18,27 +18,28 @@ export class DependabotService {
         repo: repo,
         per_page: 100,
         state: 'open',
+        tool_name: "CodeQL"
         });
 
-        let sastNumberOfSeverity1: number = 0;
-        let sastNumberOfSeverity2: number = 0
-        let sastNumberOfSeverity3: number = 0;
-        let sastNumberOfSeverity4: number = 0;
+        let SASTNumberOfSeverity1: number = 0;
+        let SASTNumberOfSeverity2: number = 0
+        let SASTNumberOfSeverity3: number = 0;
+        let SASTNumberOfSeverity4: number = 0;
 
         for await (const { data: alerts } of iterator) {
         for (const alert of alerts) {
-            switch (alert.rule.severity) {
-            case 'warning':
-                sastNumberOfSeverity1++;
+            switch (alert.rule.security_severity_level) {
+            case 'low':
+                SASTNumberOfSeverity1++;
                 break;
-            case 'note':
-                sastNumberOfSeverity2++;
+            case 'medium':
+                SASTNumberOfSeverity2++;
                 break;
-            case 'error':
-            sastNumberOfSeverity3++;
+            case 'high':
+            SASTNumberOfSeverity3++;
                 break;
-            case 'none':
-                sastNumberOfSeverity4++
+            case 'critical':
+                SASTNumberOfSeverity4++
                 break;
             default:
                 break;
@@ -46,15 +47,15 @@ export class DependabotService {
         }
         }
 
-        console.log('sastNumberOfSeverityWarning: ' + sastNumberOfSeverity1);
-        console.log('sastNumberOfSeverityNote: ' + sastNumberOfSeverity2);
-        console.log('sastNumberOfSeverityError: ' + sastNumberOfSeverity3);
-        console.log('sastNumberOfSeverityNone: ' + sastNumberOfSeverity4);
+        console.log('sastNumberOfSeveritylow: ' + SASTNumberOfSeverity1);
+        console.log('sastNumberOfSeverityMedium: ' + SASTNumberOfSeverity2);
+        console.log('sastNumberOfSeverityHigh: ' + SASTNumberOfSeverity3);
+        console.log('sastNumberOfSeverityCritical: ' + SASTNumberOfSeverity4);
 
-        core.exportVariable('SASTnumberOfSeverity1', sastNumberOfSeverity1);
-        core.exportVariable('SASTnumberOfSeverity2', sastNumberOfSeverity2);
-        core.exportVariable('SASTnumberOfSeverity3', sastNumberOfSeverity3);
-        core.exportVariable('SASTnumberOfSeverity4', sastNumberOfSeverity4);
+        core.exportVariable('SASTnumberOfSeverity1', SASTNumberOfSeverity1);
+        core.exportVariable('SASTnumberOfSeverity2', SASTNumberOfSeverity2);
+        core.exportVariable('SASTnumberOfSeverity3', SASTNumberOfSeverity3);
+        core.exportVariable('SASTnumberOfSeverity4', SASTNumberOfSeverity4);
     }
     catch(error){
         core.warning('Could not set CodeQL severities');
--- src/sasttools/SastService.ts
+++ src/sasttools/SastService.ts
@@ -1,18 +1,25 @@
 import * as core from '@actions/core';
+import { CodeQLService } from './CodeQLService';
 
 export class SastService {
   public static async getStateOfSastTool(sastTool: { nameOfTool: string }): Promise<void> {
+
     console.log('\n Running SAST control');
+    let sast = sastTool.nameOfTool;
     if (process.env.sastTool) {
-      console.log(`SAST Tool: ${process.env.sastTool}`);
-      core.exportVariable('sastTool', process.env.sastTool);
-    } else {
-      if (!sastTool.nameOfTool || sastTool.nameOfTool === 'name-of-tool') {
-        core.warning('SAST Tool is not set!');
-        return;
-      }
-      console.log(`SAST Tool: ${sastTool.nameOfTool}`);
-      core.exportVariable('sastTool', sastTool.nameOfTool);
+      sast = process.env.sastTool;
+    }
+    console.log(`SAST Tool: ${process.env.sastTool}`);
+    core.exportVariable('sastTool', process.env.sastTool);
+
+    if (!sast || sast === '' || sast === 'name-of-tool') {
+      core.warning('SAST Tool is not set!');
+      return;
+    }
+
+    if (sast.toLowerCase() === 'codeql') {
+      CodeQLService.setCodeQLFindings()
     }
   }
+
 }
