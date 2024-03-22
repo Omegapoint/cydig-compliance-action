8510003fcb8b25847143cf06e2b14428e526fb1d
hampus-andersson-op <hampus.andersson@omegapoint.se>
2024-03-22 14:02:35 +0100

    Add secret scanning functionality

---
+++ src/secretscanning/SecretScanningService.ts
@@ -0,0 +1,40 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { Octokit } from '@octokit/rest';
+import { OctokitResponse, GetResponseTypeFromEndpointMethod,
+  GetResponseDataTypeFromEndpointMethod, } from '@octokit/types';
+
+export class SecretScanningService {
+  public static async getStateOfExposedSecrets(): Promise<void> {
+    try {
+      console.log('\n Running exposed secrets control');
+      const { owner, repo }: { owner: string; repo: string } = github.context.repo;
+      const token: string = core.getInput('PAT-token');
+
+      const octokit: Octokit = new Octokit({
+        auth: token,
+      });
+      
+      type SecretAlertsForRepoResponseDataType = GetResponseDataTypeFromEndpointMethod<
+        typeof octokit.secretScanning.listAlertsForRepo
+      >;
+      
+      const iterator: AsyncIterableIterator<OctokitResponse<SecretAlertsForRepoResponseDataType>> = octokit.paginate.iterator(
+        octokit.secretScanning.listAlertsForRepo,
+        {
+          owner: owner,
+          repo: repo,
+          per_page: 100,
+          state: "open",
+        }
+      );
+      
+      let numberOfExposedSecrets: number = 0;
+      
+      for await (const {data: alerts } of iterator) {
+        numberOfExposedSecrets += alerts.length;
+      }
+      
+    } catch (error) {}
+  }
+}
