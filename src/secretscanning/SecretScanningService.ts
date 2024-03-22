7476ca546c674bc182d9c10c267017bea86aa021
hampus-andersson-op <hampus.andersson@omegapoint.se>
2024-03-22 15:07:20 +0100

    Format file

--- src/secretscanning/SecretScanningService.ts
+++ src/secretscanning/SecretScanningService.ts
@@ -1,8 +1,11 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 import { Octokit } from '@octokit/rest';
-import { OctokitResponse, GetResponseTypeFromEndpointMethod,
-  GetResponseDataTypeFromEndpointMethod, } from '@octokit/types';
+import {
+  OctokitResponse,
+  GetResponseTypeFromEndpointMethod,
+  GetResponseDataTypeFromEndpointMethod,
+} from '@octokit/types';
 
 export class SecretScanningService {
   public static async getStateOfExposedSecrets(): Promise<void> {
@@ -14,27 +17,24 @@ export class SecretScanningService {
       const octokit: Octokit = new Octokit({
         auth: token,
       });
-      
+
       type SecretAlertsForRepoResponseDataType = GetResponseDataTypeFromEndpointMethod<
         typeof octokit.secretScanning.listAlertsForRepo
       >;
-      
-      const iterator: AsyncIterableIterator<OctokitResponse<SecretAlertsForRepoResponseDataType>> = octokit.paginate.iterator(
-        octokit.secretScanning.listAlertsForRepo,
-        {
+
+      const iterator: AsyncIterableIterator<OctokitResponse<SecretAlertsForRepoResponseDataType>> =
+        octokit.paginate.iterator(octokit.secretScanning.listAlertsForRepo, {
           owner: owner,
           repo: repo,
           per_page: 100,
-          state: "open",
-        }
-      );
-      
+          state: 'open',
+        });
+
       let numberOfExposedSecrets: number = 0;
-      
-      for await (const {data: alerts } of iterator) {
+
+      for await (const { data: alerts } of iterator) {
         numberOfExposedSecrets += alerts.length;
       }
-      
     } catch (error) {}
   }
 }
