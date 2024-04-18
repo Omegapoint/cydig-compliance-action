729915e42ac7cffd55d91af78e0cd758bf866834
salemxd <uzumaki.salem@gmail.com>
2024-04-18 10:51:24 +0200

    format

--- src/identitiesInRepo/identitiesInRepoService.ts
+++ src/identitiesInRepo/identitiesInRepoService.ts
@@ -3,7 +3,6 @@ import * as github from '@actions/github';
 import { Octokit } from '@octokit/rest';
 import { GetResponseDataTypeFromEndpointMethod, OctokitResponse } from '@octokit/types';
 
-
 export class IdentitiesInRepoService {
   public static async setIdentitiesInRepoFindings(): Promise<void> {
     try {
@@ -20,12 +19,13 @@ export class IdentitiesInRepoService {
       >;
 
       // https://www.npmjs.com/package/octokit#pagination
-      const iterator: AsyncIterableIterator<OctokitResponse<listCollaboratorsForRepoResponseDataType>> = octokit.paginate.iterator(octokit.repos.listCollaborators, {
-        owner: owner,
-        repo: repo,
-        per_page: 100,
-        affiliation: 'direct',
-      });
+      const iterator: AsyncIterableIterator<OctokitResponse<listCollaboratorsForRepoResponseDataType>> =
+        octokit.paginate.iterator(octokit.repos.listCollaborators, {
+          owner: owner,
+          repo: repo,
+          per_page: 100,
+          affiliation: 'direct',
+        });
 
       let numberOfAdmins: number = 0;
       let numberOfWriters: number = 0;
