551aad302bb4c2bc4de7d2a97737e3b16f323824
salemxd <uzumaki.salem@gmail.com>
2024-05-15 09:53:03 +0200

    fix

--- src/secretscanning/SecretScanningService.ts
+++ src/secretscanning/SecretScanningService.ts
@@ -6,19 +6,23 @@ import { GithubSecretScanningService } from './GithubSecretScanningService';
 export class SecretScanningService {
   public static async getStateOfExposedSecrets(nameOfTool: string, octokit: Octokit, owner: string, repo: string): Promise<void> {
 
+    console.log('--- Secret Scanning control ---');
+
     if(nameOfTool === null || nameOfTool === 'name-of-tool'){
       core.warning('Secret Scanning Tool is not set!');
+      return;
     }
 
     switch (nameOfTool.toLowerCase()){
       case GitHub_Tools.GitHub_SECRET_SCANNING.toLowerCase():
-        GithubSecretScanningService.getStateOfExposedSecrets(octokit, owner, repo);
+        await GithubSecretScanningService.getStateOfExposedSecrets(octokit, owner, repo);
       default:
-        core.notice("Given secret scanning tool is not implemented: " + nameOfTool)
+        core.notice("Given secret scanning tool is not implemented: " + nameOfTool, {
+          title: 'Number of exposed secrets control failed',
+        })
         core.exportVariable('secretScanningTool', nameOfTool);
     }
 
     console.log();
-
   }
 }
